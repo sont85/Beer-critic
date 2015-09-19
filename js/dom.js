5b7e6c416e8b57870067993811a92d8ce166953c
Son Truong <sont85@gmail.com>
2015-09-18 20:51:44 -0700

    init for click event, input and submit button

---
+++ js/dom.js
@@ -0,0 +1,18 @@
+define(function() {
+  'use strict';
+  var $wrap = $('#beerContainer');
+
+
+  function buildSearch(data) {
+    console.log('--------',data);
+  }
+  function buildInfo() {
+  }
+
+
+
+  return {
+    buildSearch: buildSearch,
+    buildInfo: buildInfo,
+  };
+});
--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-require(['untappd'], function(untappd) {
-  // untappd.info(16630);
-  
+require(['dom', 'untappd'], function(dom, untappd) {
+  'use strict';
+  $(document).ready(function(){
+    var $searchInput = $('#searchInput');
+    var $searchBtn = $('#searchBtn');
 
-  function(init) {
-
-  }
-
-  $(document).ready(init)
-
+    function init() {
+      $searchBtn.click(function() {
+        var beerName = $searchInput.val();
+        var searchResult = untappd.search(beerName, function(data){
+          console.log(data.response.beers);
+          dom.buildSearch(data.response.beers);
+        });
+      });
+    }
+    init();
+  });
 });
