281be119dff5578c80089f201fa7c93f870ec33f
Son Truong <sont85@gmail.com>
2015-09-19 00:06:37 -0700

    build function for search

--- js/dom.js
+++ js/dom.js
@@ -1,10 +1,22 @@
+var $;
+
 define(function() {
   'use strict';
   var $wrap = $('#beerContainer');
 
 
-  function buildSearch(data) {
-    console.log('--------',data);
+  function buildSearch(beerArray) {
+    var html = beerArray.map(function(item){
+      var $beerLabel = $('<img>').attr('src', item.beer.beer_label);
+      var $beerName = $('<h4>').text(item.beer.beer_name).addClass('beer-name');
+      var $beerStyle = $('<h5>').text('Style: '+item.beer.beer_style);
+      var $brewery = $('<h5>').text(item.brewery.brewery_name);
+      var $beerListing = $('<div>').data('bid',item.beer.bid).addClass('col-xs-3 beer-div');
+      $beerListing.append($beerLabel).append($beerName).append($brewery).append($beerStyle);
+      return $beerListing;
+    });
+    console.log(html);
+    $wrap.append(html);
   }
   function buildInfo() {
   }
--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
+var $;
 require(['dom', 'untappd'], function(dom, untappd) {
   'use strict';
   $(document).ready(function(){
-    var $searchInput = $('#searchInput');
-    var $searchBtn = $('#searchBtn');
-
+    init();
+    new WOW().init();
     function init() {
+      var $searchInput = $('#searchInput');
+      var $searchBtn = $('#searchBtn');
+      var $beerContainer = $('#beerContainer');
       $searchBtn.click(function() {
         var beerName = $searchInput.val();
         var searchResult = untappd.search(beerName, function(data){
-          console.log(data.response.beers);
-          dom.buildSearch(data.response.beers);
+          console.log(data);
+          dom.buildSearch(data.response.beers.items);
         });
       });
+      $beerContainer.on('click','.beer-div', function() {
+        console.log($(this).data('bid'));
+      });
     }
-    init();
   });
 });
